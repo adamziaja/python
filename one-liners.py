579df9bd66eb0431a90596ecadefa0692f39ef07
Adam Ziaja <adamziaja@users.noreply.github.com>
2014-03-31 14:30:33 +0200

    Update one-liners.py

--- one-liners.py
+++ one-liners.py
@@ -1,4 +1,5 @@
 # python one-liners
 # (C) 2014 Adam Ziaja <adam@adamziaja.com> http://adamziaja.com
 
+# http://code.google.com/p/pefile/downloads/list
 python -c "import pefile,sys;pe=pefile.PE(sys.argv[1],fast_load=False);print pe.dump_info()" malware.exe
