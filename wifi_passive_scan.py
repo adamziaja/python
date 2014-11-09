ea5cbe675aeae9974b6ba49d62a5bf55ac4bea12
Adam Ziaja <adamziaja@users.noreply.github.com>
2014-11-09 15:23:37 +0100

    Passive scan for WiFi networks

---
+++ wifi_passive_scan.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Passive scan for WiFi networks
+
+# (C) 2014 Adam Ziaja <adam@adamziaja.com> http://adamziaja.com
+
+import sys
+from scapy.all import *
+
+unique_ssid = []
+
+def Handler(pkt):
+    if pkt.haslayer(Dot11): # 802.11
+        #if pkt.type == 0 and pkt.subtype == 8: # https://en.wikipedia.org/wiki/Beacon_frame
+        try:
+            if pkt.info not in unique_ssid and len(pkt.info) > 0:
+                unique_ssid.append(pkt.info)
+                print unique_ssid
+                print pkt.info
+                #print pkt.show()
+        except AttributeError:
+            pass
+
+sniff(iface="mon0", count=0, prn=Handler, store=0) # sudo rfkill unblock wifi && sudo airmon-ng start wlan0
