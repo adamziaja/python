752af2b81ecb25fe42e9761076f883e8d5cf18fc
Adam Ziaja <adamziaja@users.noreply.github.com>
2014-11-09 16:34:45 +0100

    WiFi probe request sniffer (passive)

--- probe_request_sniffer.py
+++ probe_request_sniffer.py
@@ -8,7 +8,7 @@
 import sys
 from scapy.all import *
 
-manuf = open('manuf.txt').read() # curl -s "https://code.wireshark.org/review/gitweb?p=wireshark.git;a=blob_plain;f=manuf;hb=HEAD" > manuf.txt
+manuf = open("manuf.txt").read() # curl -s "https://code.wireshark.org/review/gitweb?p=wireshark.git;a=blob_plain;f=manuf;hb=HEAD" > manuf.txt
 unique_probe = []
 
 def Handler(pkt):
@@ -18,7 +18,7 @@ def Handler(pkt):
             if ssid_mac not in unique_probe and len(pkt.info) > 0:
                 unique_probe.append(pkt.info + " " + pkt.addr2)
                 mac = ":".join(pkt.addr2.split(":")[:3]).upper()
-                vendor = '\n'.join(line for line in manuf.splitlines() if line.startswith(mac)).split('# ')[1]
+                vendor = "\n".join(line for line in manuf.splitlines() if line.startswith(mac)).split("# ")[1]
                 print "%s (%s %s)" % (pkt.info, pkt.addr2, vendor)
                 #print pkt.show()
 
