d84713f5af1c680b892ae06745ea6bd328a3cad1
Adam Ziaja <adamziaja@users.noreply.github.com>
2014-11-11 15:04:10 +0100

    WiFi SSID sniffer (passive)

--- ssid_sniffer.py
+++ ssid_sniffer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Passive scan for WiFi networks
+# WiFi SSID sniffer (passive)
 
 # (C) 2014 Adam Ziaja <adam@adamziaja.com> http://adamziaja.com
 
