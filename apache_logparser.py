eba67e208554fd557fc6acb02d82b7e3eded4d87
Adam Ziaja <adamziaja@users.noreply.github.com>
2016-10-08 18:15:48 +0200

    Update apache_logparser.py

--- apache_logparser.py
+++ apache_logparser.py
@@ -4,17 +4,20 @@
 # (C) 2016 Adam Ziaja <adam@adamziaja.com> http://adamziaja.com
 
 import re
-regex = '([(\d\.)]+) (\w+|-) (\w+|-) \[(.*?)\] "(.*?)" (\d+) (\d+|-) "(.*?)" "(.*?)"' # LogFormat "%h %l %u %t \"%r\" %>s %O \"%{Referer}i\" \"%{User-Agent}i\"" combined
+
+# LogFormat "%h %l %u %t \"%r\" %>s %O \"%{Referer}i\" \"%{User-Agent}i\"" combined
+regex = '([(\d\.)]+) (\w+|-) (\w+|-) \[(.*?)\] "(.*?)" (\d+) (\d+|-) "(.*?)" "(.*?)"'
 
 with open('/var/log/access.log') as f:
 	for line in f:
 		print re.match(regex, line).groups()
-		print re.match(regex, line).groups()[0] # %h	Remote hostname.
-		print re.match(regex, line).groups()[1] # %l	Remote logname (from identd, if supplied).
-		print re.match(regex, line).groups()[2] # %u	Remote user if the request was authenticated.
-		print re.match(regex, line).groups()[3] # %t	Time the request was received.
-		print re.match(regex, line).groups()[4] # %r	First line of request.
-		print re.match(regex, line).groups()[5] # %s	Status.
-		print re.match(regex, line).groups()[6] # %O	Bytes sent, including headers.
-		print re.match(regex, line).groups()[7] # %{Referer}i
-		print re.match(regex, line).groups()[8] # %{User-agent}i
+		hostname = re.match(regex, line).groups()[0] # %h	Remote hostname.
+		# re.match(regex, line).groups()[1] # %l	Remote logname (from identd, if supplied).
+		# re.match(regex, line).groups()[2] # %u	Remote user if the request was authenticated.
+		time = re.match(regex, line).groups()[3] # %t	Time the request was received.
+		request = re.match(regex, line).groups()[4] # %r	First line of request.
+		status = re.match(regex, line).groups()[5] # %s	Status.
+		# re.match(regex, line).groups()[6] # %O	Bytes sent, including headers.
+		referer = re.match(regex, line).groups()[7] # %{Referer}i
+		useragent = re.match(regex, line).groups()[8] # %{User-Agent}i
+		print hostname, time, request, status, referer, useragent
