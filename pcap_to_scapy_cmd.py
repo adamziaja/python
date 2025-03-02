edd77d44f3d92329d540f64f4841f059b7bba35f
Adam Ziaja <adamziaja@users.noreply.github.com>
2025-03-02 10:19:37 +0100

    Update pcap_to_scapy_cmd.py

--- pcap_to_scapy_cmd.py
+++ pcap_to_scapy_cmd.py
@@ -1,15 +1,13 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
+#!/usr/bin/env python3
 # PCAP to Scapy commands
-
-# (C) 2014 Adam Ziaja <adam@adamziaja.com> http://adamziaja.com
+# (C) 2014,2025 Adam Ziaja <adam@adamziaja.com> http://adamziaja.com
 
 from scapy.all import *
 
 def Handler(pkt):
-    #print pkt.show()
-    #print pkt.summary()
-    print pkt.command()
+    #print(pkt.show())
+    #print(pkt.summary())
+    print(pkt.command())
+    print(pkt.command()).hide_defaults()
     
 sniff(offline=sys.argv[1], prn=Handler)
